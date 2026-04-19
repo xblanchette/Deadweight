e292417a991b729af125944732e00294db17b6e9
LAPTOP-GP6E07VB\adlouto <a.berraki@student.isartdigital.com>
2026-04-18 23:51:58 -0400

    change soundManager

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -7,6 +7,7 @@ public class SoundManager : MonoBehaviour
 {
 
     public static SoundManager instance;
+    public AudioSource sound;
     public List<AudioClip> audioClips = new List<AudioClip>();
 
 
